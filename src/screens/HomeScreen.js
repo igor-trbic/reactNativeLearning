45378bdfad771b98eea03a988eda701136836507
Igor Trbic <igor.trbic@digitaz.rs>
2020-02-21 14:55:13 +0100

    Added buttons ad navigation, YAY!

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
-const HomeScreen = () => <Text style={styles.text}>Yes this is a dog!</Text>;
-
+const HomeScreen = ({ navigation }) => (
+    <View>
+      <Text style={styles.text}>Yes this is a dog!</Text>
+      <Button
+        title="Go to components DEMO"
+        style={styles.button}
+        onPress={() => navigation.navigate('Components')}
+      />
+      <Button
+        title="Go to List DEMO"
+        style={styles.button}
+        onPress={() => navigation.navigate('List')}
+      />
+    </View>
+);
 const styles = StyleSheet.create({
+  button: {
+    paddingBottom: 10
+  },
   text: {
     fontSize: 30
   }
