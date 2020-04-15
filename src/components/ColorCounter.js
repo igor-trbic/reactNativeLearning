c35d0dc5ec6a559e0d4f32e7dfdec599a74b41fb
Igor Trbic <trbic.igor@gmail.com>
2020-04-16 01:28:18 +0200

    More state, pre reducer

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,13 +1,17 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { Text, StyleSheet, Image, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
-const ColorCounter = ({ color }) => (
+const ColorCounter = ({ color, onIncrease, onDecrease }) => (
     
     <View>
         <Text>{color}</Text>
-        <Button title={`Increase ${color}`} />
-        <Button title={`Decrease ${color}`} />
+        <Button 
+            onPress={() => onIncrease()}
+            title={`Increase ${color}`} />
+        <Button 
+            onPress={() => onDecrease()}
+            title={`Decrease ${color}`} />
     </View>
     
 );
